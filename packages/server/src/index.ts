30f8a71b3ef51c65afeaeb82b264de39135b852b
Edgar Garcia <97008141+EdgarJGarcia@users.noreply.github.com>
2025-11-10 23:56:17 -0800

    added login

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { connect } from "./services/mongo";
 import photosRouter from "./routes/photos";
+import auth, { authenticateUser } from "./routes/auth";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +10,11 @@ const staticDir = process.env.STATIC || "public";
 connect("photography");
 
 app.use(express.static(staticDir));
-
 app.use(express.json());
 
-app.use("/api/photos", photosRouter);
+app.use("/auth", auth);
+
+app.use("/api/photos", authenticateUser, photosRouter);
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
@@ -21,3 +23,4 @@ app.get("/hello", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
